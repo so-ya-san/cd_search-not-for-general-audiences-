27a3fb22adcab8f4ccb4d1aacd96c9e575196d19
so-ya-san <sogaya0203@outlook.jp>
2025-07-06 20:47:05 +0900

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-    base: '/cd_search-not-for-general-audiences-git/', // ✅ ← ここに追加！
-    plugins: [react()],
-})
