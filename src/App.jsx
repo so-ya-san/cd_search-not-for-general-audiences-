bccbf2f95640f5f38b67e6e6bafff2403c266e7e
so-ya-san <sogaya0203@outlook.jp>
2025-07-06 15:07:24 +0900

    初回コミット

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import TrackSearch from './TrackSearch';
+
+function App() {
+    return (
+        <div className="outer-container">
+            <TrackSearch />
+        </div>
+    );
+}
+
+export default App;
